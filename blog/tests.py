60319ce355548b103270844ee5ce85702777b250
Nathan Graule <solarliner@gmail.com>
2018-01-07 00:43:06 +0100

    Tests now using new manager 'profiles'

--- blog/tests.py
+++ blog/tests.py
@@ -6,15 +6,13 @@ from .models import Profile
 
 class ProfileTest(TestCase):
     def setUp(self):
-        self.john = Profile.objects.create_user(
+        self.john = Profile.profiles.create_user(
             'johndoe', 'john@gmail.com', None, first_name="John", last_name="Doe")
-        self.solar = Profile.objects.create_user(
+        self.solar = Profile.profiles.create_user(
             'solarliner', 'solarliner@gmail.com', None, first_name="Nathan", last_name="Graule")
 
     def test_full_name(self):
         """Profile has a `full_name` property that returns the full name."""
-        # john = Profile.objects.get_by_natural_key('johndoe')
-        # solar = Profile.objects.get_by_natural_key('solarliner')
         self.assertEqual(self.john.full_name, "John Doe")
         self.assertEqual(self.solar.full_name, "Nathan Graule")
 
