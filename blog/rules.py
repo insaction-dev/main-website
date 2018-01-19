405f0994fab8e76de93a7e4886c068c9655eb6bb
Nathan Graule <solarliner@gmail.com>
2018-01-19 23:10:04 +0100

    Added is_author_group to permissions

--- blog/rules.py
+++ blog/rules.py
@@ -3,13 +3,13 @@ from insaction.rules import is_author, is_author_group, is_editor, is_manager
 
 rules.add_perm('blog', rules.always_allow)
 rules.add_perm('blog.create_article',
-               is_author_group | is_editor | is_manager |
+               is_author_group |
                rules.is_superuser)
 rules.add_perm('blog.change_article',
-               is_author_group | is_author | is_manager |
+               is_author | is_editor |
                rules.is_superuser)
 rules.add_perm('blog.delete_article',
-               is_author | is_editor | is_manager |
+               is_author | is_manager |
                rules.is_superuser)
 
 rules.add_perm('blog.create_category',
