6887e2747c6f72ea63d1e17055ea22612230575b
Nathan Graule <solarliner@gmail.com>
2018-01-19 22:57:03 +0100

    django rules now include previous hierarchy directly

--- insaction/rules.py
+++ insaction/rules.py
@@ -11,5 +11,5 @@ def is_author(self, user, authored_object):
 
 
 is_author_group = rules.is_group_member('author')
-is_editor = rules.is_group_member('editor')
-is_manager = rules.is_group_member('manager')
+is_editor = rules.is_group_member('editor') | is_author
+is_manager = rules.is_group_member('manager') | is_editor
